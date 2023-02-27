6582d4c6dbe381ca3e78439358ba8a5eeb44dbb3
Tinashe Chitakunye <tinashec93@live.com>
2023-02-27 15:34:48 +0200

    Initial page setup done

---
+++ Pages/Showcase.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheasbyteSite.Pages
+{
+    public class ShowcaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
